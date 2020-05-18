078fe4652b9214784c8087b7a651a2ac39912b52
yu-kurih <yu.k.development@gmail.com>
2020-05-18 02:36:15 +0000

    reintroduce

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
   @if(Auth::check())
-    {{ Auth::user()->name }}
+    <div class="text-center">
+      {!! link_to_route('user.home', 'ユーザホームへ移動', [], ['class' => 'btn btn-lg btn-success']) !!}
+    <div>  
   @else
     <div class="text-center">
       <img src="/images/machipocket.png" alt="machipocket" class="img-fluid rounded mb-4"></br>
